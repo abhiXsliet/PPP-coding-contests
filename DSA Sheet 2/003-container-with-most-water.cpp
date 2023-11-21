697cb1a59e385fb999fb923bf8940c383816a0bb
Abhishek Kumar <abhixsliet@gmail.com>
2023-11-21 20:35:45 +0530

    DSA Sheet - 2 : 14th-November-2023 files added

---
+++ DSA Sheet 2/003-container-with-most-water.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/container-with-most-water/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // TC = O(N)
+    // SC = O(1)
+    int maxArea(vector<int>& arr) {
+        int n = arr.size();
+        
+        int maxArea = INT_MIN;
+        
+        int j = n - 1;
+        int i = 0;
+        
+        while (i < j) {
+            if (arr[i] > arr[j]) {
+                maxArea = max(maxArea, arr[j]*(j - i));
+                j--;
+            }
+            else {
+                maxArea = max(maxArea, arr[i] * (j - i));
+                i++;
+
+            }
+        }
+        return maxArea;
+    }
+};
